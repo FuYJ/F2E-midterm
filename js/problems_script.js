a12ab7301f3f8389a5ae6f4f5303397b6888311d
FuYJ <fuyiru@fuyirudeMacBook-Pro.local>
2016-04-15 19:26:32 +0800

    modify links hover effect

--- js/problems_script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.link_block_normal').mouseenter(function(){
-        $(this).addClass('link_block_hover');
-    });
-    $('.link_block_normal').mouseleave(function(){
-        $(this).removeClass('link_block_hover');
-    });
-});
